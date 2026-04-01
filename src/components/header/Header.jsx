606cb413c82f4a01e19c08ec5654b5a33f2c54d3
Nivea-izeLu <niveapontes02@gmail.com>
2026-04-01 15:26:39 -0300

    css do header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,12 @@
+import "./header.css"; 
 
 export default function Header () {
 
     return (
 
-        <header>
-            <h1> Sistema Listagem de Alunos</h1>
-            <h3>Projeto feito com <i>React</i></h3>
+        <header class="container">
+            <h1 class="titulo"> Sistema Listagem de Alunos</h1>
+            <h3 class="sub">Projeto feito com <i>React</i></h3>
         </header>
     )
 }
